fdd5e4d64ba1d4e8333ed14dd4539b743c81b2bf
chenc <demoncraz@macbook-pro-2.local>
2018-09-28 16:29:09 +0800

    updated with more customization options

--- CCPickerView/CCPickerView.h
+++ CCPickerView/CCPickerView.h
@@ -13,6 +13,7 @@
 
 - (NSInteger)numberOfRowsInPickerView:(CCPickerView *)pickerView;
 - (NSString *)pickerView:(CCPickerView *)pickerView titleForRow:(NSInteger)row;
+- (UIView *)pickerView:(CCPickerView *)pickerView viewForRow:(NSInteger)row;   //if view is provided, titleForRow will be ignored.
 
 @end
 
@@ -24,15 +25,32 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+typedef NS_ENUM(NSUInteger, CCPickerViewFadeOutLevel) {
+    CCPickerViewFadeOutLevelMinimum,
+    CCPickerViewFadeOutLevelMiddle,
+    CCPickerViewFadeOutLevelMax,
+};
+
 @interface CCPickerView : UIView
 
 @property (nonatomic, weak) id<CCPickerViewDataSource> dataSource;
 @property (nonatomic, weak) id<CCPickerViewDelegate> delegate;
 
-@property (nonatomic, assign) BOOL showSelectionIndicator;
-@property (nonatomic, strong) UIColor *selectedColor;
-@property (readonly, nonatomic, assign) NSInteger selectedIndex;
+@property (nonatomic, assign) BOOL showSelectionIndicator;            //Shows a indicator box around the selected row, default is NO
+@property (nonatomic, assign) BOOL allowSelectionFeedback;            //Defualt is True, there will be vibrate upon selection
+@property (nonatomic, strong) UIColor *selectedColor;                 //Text color of the selected row, default is predifined BLUE
+@property (nonatomic, assign) NSUInteger selectedFontSize;            //Font size of the selected row, default is 25
+@property (nonatomic, assign) NSUInteger maxDisplayingRows;           //Max number of rows that can be displayed at the same time, should be a odd number, equal or larger than 3; Default is 5
+@property (nonatomic, assign) CCPickerViewFadeOutLevel fadeOutLevel;  //The level of fade out effect, i.e. MAX means the unselected rows fades out faster. Default is CCPickerViewFadeOutLevelMiddle
 
+@property (readonly, nonatomic, assign) NSInteger selectedIndex;      //The current selected index
+
+/**
+ Select a row with(out) animation
+
+ @param row the desitination row
+ @param animated if animated
+ */
 - (void)selectRow:(NSInteger)row animated:(BOOL)animated;
 
 @end
--- CCPickerView/CCPickerView.m
+++ CCPickerView/CCPickerView.m
@@ -7,7 +7,6 @@
 //
 
 #import "CCPickerView.h"
-#import "CCPickerViewFlowLayout.h"
 
 static NSString * const kPickerCollectionCell = @"kPickerCollectionCell";
 
@@ -16,9 +15,12 @@ static const CGFloat kMinimumLineSpacing = -2.0f;
 
 @interface CCPickerViewCell: UICollectionViewCell
 
-@property (nonatomic, strong) UILabel *label;
+@property (nonatomic, strong) UIView    *view;
+@property (nonatomic, strong) UILabel   *label;
 
 @end
+
+
 @implementation CCPickerViewCell
 
 - (instancetype)initWithFrame:(CGRect)frame
@@ -27,17 +29,139 @@ static const CGFloat kMinimumLineSpacing = -2.0f;
     if (self) {
         UILabel *label = [[UILabel alloc] init];
         self.label = label;
-        label.font = [UIFont systemFontOfSize:23];
         label.textAlignment = NSTextAlignmentCenter;
-        label.backgroundColor = [UIColor whiteColor];
         [self.contentView addSubview:label];
     }
     return self;
 }
 
+- (void)setView:(UIView *)view {
+    if (_view && _view.superview) {
+        [_view removeFromSuperview];
+    }
+    _view = view;
+    view.frame = self.contentView.bounds;
+    [self.contentView addSubview:view];
+}
+
 - (void)layoutSubviews {
     [super layoutSubviews];
     self.label.frame = self.contentView.bounds;
+    self.view.center = CGPointMake(self.contentView.bounds.size.width * 0.5, self.contentView.bounds.size.height * 0.5);
+}
+
+@end
+
+@interface CCPickerViewFlowLayout: UICollectionViewFlowLayout
+
+@property (nonatomic, assign) CCPickerViewFadeOutLevel fadeOutLevel;
+
+@property (nonatomic, assign) NSUInteger maxDisplayingRows;
+
+@property (nonatomic, assign) BOOL isSetup;
+
+@end
+
+@implementation CCPickerViewFlowLayout
+
+- (instancetype)init {
+    if (self = [super init]) {
+        self.fadeOutLevel = CCPickerViewFadeOutLevelMiddle;
+        self.maxDisplayingRows = 5;
+    }
+    return self;
+}
+
+- (void)prepareLayout {
+    [super prepareLayout];
+    if (self.isSetup == NO) {
+        [self setupCollectionView];
+        self.isSetup = YES;
+    }
+}
+
+- (NSArray<UICollectionViewLayoutAttributes *> *)layoutAttributesForElementsInRect:(CGRect)rect {
+    NSArray<UICollectionViewLayoutAttributes *> *attributes = [super layoutAttributesForElementsInRect:rect];
+    
+    NSMutableArray<UICollectionViewLayoutAttributes *> *attributesCopy = [NSMutableArray array];
+    
+    for (UICollectionViewLayoutAttributes *itemAttributes in attributes) {
+        UICollectionViewLayoutAttributes *itemAttributesCopy = [itemAttributes copy];
+        [self changeLayoutAttributes:itemAttributesCopy];
+        [attributesCopy addObject:itemAttributesCopy];
+    }
+    return attributesCopy;
+}
+
+- (BOOL)shouldInvalidateLayoutForBoundsChange:(CGRect)newBounds {
+    return YES;
+}
+
+- (void)changeLayoutAttributes:(UICollectionViewLayoutAttributes *)attributes {
+    CGFloat collectionCenter = self.collectionView.frame.size.height * 0.5;
+    CGFloat offset = self.collectionView.contentOffset.y;
+    CGFloat normalizedCenter = attributes.center.y - offset;
+    
+    CGFloat maxDistance = 2 * (self.itemSize.height + self.minimumLineSpacing);
+    CGFloat distance = MIN(ABS(collectionCenter - normalizedCenter), maxDistance);
+    
+    //计算跟中间行的行差
+    NSUInteger rowDiff = floor(ABS(collectionCenter - normalizedCenter) / kStandardItemHeight);
+    NSUInteger maxRowDiff = self.maxDisplayingRows / 2;
+    
+    CGFloat ratio = (maxDistance - distance) / maxDistance;
+    
+    CGFloat standardItemAlpha = 0.5;
+    CGFloat standardItemScale = 0.5;
+    switch (self.fadeOutLevel) {
+        case CCPickerViewFadeOutLevelMiddle:
+            break;
+        case CCPickerViewFadeOutLevelMinimum:
+            standardItemAlpha = 0.75;
+            standardItemScale = 0.75;
+            break;
+        case CCPickerViewFadeOutLevelMax:
+            standardItemAlpha = 0.25;
+            standardItemScale = 0.25;
+            break;
+    }
+    
+    CGFloat alpha = ratio * (1 - standardItemAlpha) + standardItemAlpha;
+    CGFloat scale = ratio * (1 - standardItemScale) + standardItemScale;
+    
+    if (rowDiff >= maxRowDiff) {
+        alpha = 0.0;
+        scale = 0.0;
+    }
+    
+    attributes.alpha = alpha;
+    
+    attributes.transform3D = CATransform3DScale(CATransform3DIdentity, scale, scale, 1);
+    
+    attributes.zIndex = (NSInteger)alpha * 10;
+}
+
+- (CGPoint)targetContentOffsetForProposedContentOffset:(CGPoint)proposedContentOffset withScrollingVelocity:(CGPoint)velocity {
+    NSArray *layoutAttributes = [self layoutAttributesForElementsInRect:self.collectionView.bounds];
+    CGFloat center = self.collectionView.bounds.size.height * 0.5;
+    CGFloat proposedConetentOffsetCenterOrigin = proposedContentOffset.y + center;
+    NSArray *sorted = [layoutAttributes sortedArrayUsingComparator:^NSComparisonResult(UICollectionViewLayoutAttributes * _Nonnull obj1, UICollectionViewLayoutAttributes *  _Nonnull obj2) {
+        if (ABS(obj1.center.y - proposedConetentOffsetCenterOrigin) <= ABS(obj2.center.y - proposedConetentOffsetCenterOrigin)) {
+            return NSOrderedAscending;
+        }
+        return NSOrderedDescending;
+    }];
+    UICollectionViewLayoutAttributes * closest = sorted.firstObject ? : [[UICollectionViewLayoutAttributes alloc] init];
+    CGPoint targetContentOffset = CGPointMake(proposedContentOffset.x, floor(closest.center.y - center));
+    return targetContentOffset;
+}
+
+- (void)setupCollectionView {
+    self.collectionView.decelerationRate = UIScrollViewDecelerationRateFast;
+    CGSize collectionSize = self.collectionView.bounds.size;
+    CGFloat yInset = (collectionSize.height - self.itemSize.height) * 0.5;
+    CGFloat xInset = (collectionSize.width - self.itemSize.width) * 0.5;
+    self.sectionInset = UIEdgeInsetsMake(yInset, xInset, yInset, xInset);
 }
 
 @end
@@ -71,6 +195,7 @@ static const CGFloat kMinimumLineSpacing = -2.0f;
     self = [super initWithFrame:frame];
     if (self) {
         [self setupUI];
+        self.allowSelectionFeedback = YES;
         self.feedbackGenerator = [[UISelectionFeedbackGenerator alloc] init];
         [self.feedbackGenerator prepare];
     }
@@ -90,20 +215,26 @@ static const CGFloat kMinimumLineSpacing = -2.0f;
     _collectionView.delegate = self;
     _collectionView.backgroundColor = [UIColor whiteColor];
     _collectionView.showsVerticalScrollIndicator = NO;
+    _collectionView.bounces = NO;
     [_collectionView registerClass:[CCPickerViewCell class] forCellWithReuseIdentifier:kPickerCollectionCell];
     [self addSubview:_collectionView];
+}
+
+- (void)setBackgroundColor:(UIColor *)backgroundColor {
+    [super setBackgroundColor:backgroundColor];
+    self.collectionView.backgroundColor = backgroundColor;
 }
 
 - (void)layoutSubviews {
     [super layoutSubviews];
     self.collectionView.frame = self.bounds;
     if (_showSelectionIndicator) {
-        _indicatorView.frame = CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width + 2, 34);
+        _indicatorView.frame = CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width + 2, kStandardItemHeight + 1);
         _indicatorView.center = CGPointMake(self.bounds.size.width * 0.5, self.bounds.size.height * 0.5);
     }
 }
 
-#pragma mark - Public property
+#pragma mark - Public properties
 - (void)setShowSelectionIndicator:(BOOL)showSelectionIndicator {
     _showSelectionIndicator = showSelectionIndicator;
     if (showSelectionIndicator) {
@@ -113,10 +244,35 @@ static const CGFloat kMinimumLineSpacing = -2.0f;
     }
 }
 
+- (void)setSelectedColor:(UIColor *)selectedColor {
+    _selectedColor = selectedColor;
+    [self.collectionView reloadData];
+}
+
+- (void)setSelectedFontSize:(NSUInteger)selectedFontSize {
+    _selectedFontSize = selectedFontSize;
+    [self.collectionView reloadData];
+}
+
+- (void)setFadeOutLevel:(CCPickerViewFadeOutLevel)fadeOutLevel {
+    _fadeOutLevel = fadeOutLevel;
+    CCPickerViewFlowLayout *layout = (CCPickerViewFlowLayout *)self.collectionView.collectionViewLayout;
+    layout.fadeOutLevel = fadeOutLevel;
+}
+
+- (void)setMaxDisplayingRows:(NSUInteger)maxDisplayingRows {
+    _maxDisplayingRows = maxDisplayingRows;
+    NSAssert(maxDisplayingRows % 2 == 1 && maxDisplayingRows >= 3, @"Max displaying row number should be Odd and >= 3");
+    CCPickerViewFlowLayout *layout = (CCPickerViewFlowLayout *)self.collectionView.collectionViewLayout;
+    layout.maxDisplayingRows = maxDisplayingRows;
+}
+
+#pragma mark - Public functions
 - (void)selectRow:(NSInteger)row animated:(BOOL)animated {
     [self.collectionView setContentOffset:CGPointMake(0, (kMinimumLineSpacing + kStandardItemHeight) * row) animated:animated];
     self.selectedIndex = row;
 }
+
 
 - (void)setSelectedIndex:(NSInteger)selectedIndex {
     _selectedIndex = selectedIndex;
@@ -124,13 +280,11 @@ static const CGFloat kMinimumLineSpacing = -2.0f;
         [self.delegate pickerView:self didSelectRow:selectedIndex];
     }
     [self.collectionView reloadData];
-    [self.feedbackGenerator selectionChanged];
+    if (self.allowSelectionFeedback) {
+        [self.feedbackGenerator selectionChanged];
+    }
 }
 
-- (void)setSelectedColor:(UIColor *)selectedColor {
-    _selectedColor = selectedColor;
-    [self.collectionView reloadData];
-}
 
 #pragma mark - UICollectionView
 - (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
@@ -146,15 +300,25 @@ static const CGFloat kMinimumLineSpacing = -2.0f;
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
     CCPickerViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:kPickerCollectionCell forIndexPath:indexPath];
-    if (self.dataSource && [self.dataSource respondsToSelector:@selector(pickerView:titleForRow:)]) {
-        NSString *title = [self.dataSource pickerView:self titleForRow:indexPath.item];
-        cell.label.text = title;
-        UIColor *selectedColor = self.selectedColor ? : [UIColor colorWithRed:33.0 / 255.0 green:150.0 / 255.0 blue:243.0 / 255.0 alpha:1.0];
-        cell.label.textColor = indexPath.item == self.selectedIndex ? selectedColor : [UIColor blackColor];
-    }
     
+    if (self.dataSource && [self.dataSource respondsToSelector:@selector(pickerView:viewForRow:)]) {
+        UIView *view = [self.dataSource pickerView:self viewForRow:indexPath.item];
+        cell.view = view;
+        return cell;
+    } else {
+        if (self.dataSource && [self.dataSource respondsToSelector:@selector(pickerView:titleForRow:)]) {
+            NSString *title = [self.dataSource pickerView:self titleForRow:indexPath.item];
+            cell.label.text = title;
+            UIColor *selectedColor = self.selectedColor ? : [UIColor colorWithRed:33.0 / 255.0 green:150.0 / 255.0 blue:243.0 / 255.0 alpha:1.0];
+            cell.label.textColor = indexPath.item == self.selectedIndex ? selectedColor : [UIColor blackColor];
+            NSUInteger fontSize = self.selectedFontSize == 0 ? 25 : self.selectedFontSize;
+            cell.label.font = [UIFont systemFontOfSize:fontSize];
+        }
+    }
     return cell;
 }
+
+#pragma mark - UIScrollViewDelegate
 
 - (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
     CCPickerViewFlowLayout *layout = (CCPickerViewFlowLayout *)self.collectionView.collectionViewLayout;
